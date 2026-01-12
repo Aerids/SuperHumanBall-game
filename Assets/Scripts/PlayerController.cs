f2d355e36573a5a2ca4c849b776bf9788b0dc11e
Aerids <eridsdale17@gmail.com>
2026-01-12 16:17:45 -0500

    Big jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public int jumpHeight = 1;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpHeight, rb.linearVelocity.x);
+        }
+    }
+}
